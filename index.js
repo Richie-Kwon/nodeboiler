fcb6cc8d97bf48eedc5ea352e004d664392dcb42
richiekwon <richie.kwon1@gmail.com>
2021-06-13 10:59:22 +0100

    token

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const port = 3000;
 
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const { User } = require("./models/Users");
 const config = require("./config/key");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose
   .connect(config.mongoURI, {
@@ -29,6 +31,28 @@ app.post("/register", (req, res) => {
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({ loginSuceess: false, message: "No user found" });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSucess: false,
+          message: "password is incorrect",
+        });
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x-auth", user.token)
+          .status(200)
+          .json({ loginSuceess: true, userId: user._id });
+      });
+    });
   });
 });
 
